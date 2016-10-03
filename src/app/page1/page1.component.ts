d3a6557435715f11a8c7fc64eda46b3bd4397ea5
ovrmrw <t.ngc.0403@gmail.com>
2016-10-03 12:06:06 +0900

    Fix: template

--- src/app/page1/page1.component.ts
+++ src/app/page1/page1.component.ts
@@ -12,10 +12,12 @@ import { State } from '../store';
     <h5>{{counterEvery | asyncState}}</h5>
     <h4>Counter (switchMap)</h4>
     <h5>{{counterLatest | asyncState}}</h5>
+    <hr />
     <h4>Time (switchMap)</h4>
     <h5>{{timeSerial | asyncState | date:"medium"}}</h5>
     <h4>Math (mergeMap) without Firebase sync</h4>
     <h5>addition: {{addition | asyncState}}, subtraction: {{subtraction | asyncState}}, multiplication: {{multiplication | asyncState}}</h5>
+    <hr />
     <button (click)="increment()" class="btn btn-primary" id="increment-btn">+</button>
     <button (click)="decrement()" class="btn btn-primary" id="decrement-btn">-</button>
     <button (click)="reset()" class="btn btn-warning" id="reset-btn">RESET</button>
@@ -57,12 +59,15 @@ export class Page1Component {
   // get counterEvery(): Observable<number> { return this.state.getState().map(s => s.increment.counter); }
   // get counterLatest(): Observable<number> { return this.state.getState().map(s => s.increment.counter); }
   // get timeSerial(): Observable<number> { return this.state.getState().map(s => s.time.serial); }
+  // get addition(): Observable<number> { return this.state.getState().map(s => s.math.addition); }
+  // get subtraction(): Observable<number> { return this.state.getState().map(s => s.math.subtraction); }
+  // get multiplication(): Observable<number> { return this.state.getState().map(s => s.math.multiplication); }
   // get actionName(): Observable<string> { return this.state.getState().map(s => s.actionName); }
 
   /* 下記のように書くと、counterとtimeSerialは"それぞれのPromiseが解決したタイミングで"Viewが更新される。 */
   get counterEvery(): Observable<number> { return this.state.incrementStateEvery$.map(s => s.counter); }
   get counterLatest(): Observable<number> { return this.state.incrementStateLatest$.map(s => s.counter); }
-  get timeSerial(): Observable<number> { return this.state.timeStateLatest$.map(s => s.serial); }  
+  get timeSerial(): Observable<number> { return this.state.timeStateLatest$.map(s => s.serial); }
   get addition(): Observable<number> { return this.state.mathStateEvery$.map(s => s.addition); }
   get subtraction(): Observable<number> { return this.state.mathStateEvery$.map(s => s.subtraction); }
   get multiplication(): Observable<number> { return this.state.mathStateEvery$.map(s => s.multiplication); }
